191873e6794b1b28bfa963af2d855478eaa018c6
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-03-28 12:38:43 +0200

    handle lost of focus

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,10 +8,11 @@ function main() {
     const plotter = new Plotter();
     const rope = new Rope(10, 40);
 
+    const maxDt = 1 / 60;
     let lastUpdate = performance.now();
     function mainLoop() {
         const now = performance.now();
-        const dt = 0.001 * (now - lastUpdate);
+        const dt = Math.min(maxDt, 0.001 * (now - lastUpdate));
         lastUpdate = now;
         rope.update(dt, Parameters.mousePositionInPixels);
 
